cf6932654feeeea8acb677c864a64840d8d4ca17
Sergey <boboshkocb@gmail.com>
2018-10-18 19:02:45 +0300

    Add methods

--- bcv/hw/Lection9/XOGame.java
+++ bcv/hw/Lection9/XOGame.java
@@ -18,12 +18,15 @@ public class XOGame {
 }
 
 class XOGameFrame extends JFrame {
+    //size of gameboard
+    public static final int rowsBoard = 4, colsBoard = 4;
+
     // Indicate whose turn it is
     private char whoseTurn = 'X';
     private boolean gameOver = false;
 
     // Create cell grid
-    private Cell[][] cells = new Cell[4][4];
+    private Cell[][] cells = new Cell[rowsBoard][colsBoard];
 
     // Create a status label
     JLabel jlStatusBar = new JLabel("X's turn to play");
@@ -33,10 +36,10 @@ class XOGameFrame extends JFrame {
      */
     public XOGameFrame() {
         // Panel to hold cells
-        JPanel panel = new JPanel(new GridLayout(4, 4, 0, 0));
-        for (int i = 0; i < 4; i++)
-            for (int j = 0; j < 4; j++)
-                panel.add(cells[i][j] = new Cell());
+        JPanel panel = new JPanel(new GridLayout(rowsBoard, colsBoard, 0, 0));
+        for (int rows = 0; rows < rowsBoard; rows++)
+            for (int cols = 0; cols < colsBoard; cols++)
+                panel.add(cells[rows][cols] = new Cell());
 
         panel.setBorder(new LineBorder(Color.red, 2));
 
@@ -44,7 +47,7 @@ class XOGameFrame extends JFrame {
         jlStatusBar.setBorder(new LineBorder(Color.yellow, 2));
 
         add(panel, BorderLayout.CENTER);
-        add(jlStatusBar, BorderLayout.SOUTH);
+        add(jlStatusBar, BorderLayout.NORTH);
     }
 
     /**
@@ -53,9 +56,9 @@ class XOGameFrame extends JFrame {
      * @return True, if game board is full. Otherwise, false.
      */
     public boolean isFull() {
-        for (int i = 0; i < 4; i++)
-            for (int j = 0; j < 4; j++)
-                if (cells[i][j].getToken() == ' ')
+        for (int rows = 0; rows < rowsBoard; rows++)
+            for (int cols = 0; cols < colsBoard; cols++)
+                if (cells[rows][cols].getToken() == ' ')
                     return false;
         return true;
     }
@@ -68,20 +71,20 @@ class XOGameFrame extends JFrame {
      */
     public boolean isWon(char token) {
         // check rows
-        for (int i = 0; i < 4; i++)
-            if ((cells[i][0].getToken() == token)
-                    && (cells[i][1].getToken() == token)
-                    && (cells[i][2].getToken() == token)
-                    && (cells[i][3].getToken() == token)) {
+        for (int rows = 0; rows < rowsBoard; rows++)
+            if ((cells[rows][0].getToken() == token)
+                    && (cells[rows][1].getToken() == token)
+                    && (cells[rows][2].getToken() == token)
+                    && (cells[rows][3].getToken() == token)) {
                 return true;
             }
 
         // check columns
-        for (int j = 0; j < 3; j++)
-            if ((cells[0][j].getToken() == token)
-                    && (cells[1][j].getToken() == token)
-                    && (cells[2][j].getToken() == token)
-                    && (cells[3][j].getToken() == token)) {
+        for (int cols = 0; cols < colsBoard; cols++)
+            if ((cells[0][cols].getToken() == token)
+                    && (cells[1][cols].getToken() == token)
+                    && (cells[2][cols].getToken() == token)
+                    && (cells[3][cols].getToken() == token)) {
                 return true;
             }
         // check diagonal
@@ -93,7 +96,7 @@ class XOGameFrame extends JFrame {
         }
 
         if ((cells[0][3].getToken() == token)
-                && (cells[1][1].getToken() == token)
+                && (cells[1][2].getToken() == token)
                 && (cells[2][1].getToken() == token)
                 && (cells[3][0].getToken() == token)) {
             return true;
@@ -143,12 +146,12 @@ class XOGameFrame extends JFrame {
             if (token == 'X') {
                 //  g.drawLine(20, 20, getWidth() - 20, getHeight() - 20);
                 //  g.drawLine(getWidth() - 20, 20, 20, getHeight() - 20);
-                Image aXImage = new ImageIcon("C:\\Users\\home\\IdeaProjects\\bcv\\src\\com\\bcv\\hw\\Lection9\\x.gif").getImage();
+                Image aXImage = new ImageIcon("src\\com\\bcv\\hw\\Lection9\\x.gif").getImage();
                 g.drawImage(aXImage, 0, 0, getHeight(), getHeight(), null);
 
             } else if (token == 'O') {
                 // g.drawOval(10, 10, getWidth() - 20, getHeight() - 20);
-                Image aOImage = new ImageIcon("C:\\Users\\home\\IdeaProjects\\bcv\\src\\com\\bcv\\hw\\Lection9\\o.gif").getImage();
+                Image aOImage = new ImageIcon("src\\com\\bcv\\hw\\Lection9\\o.gif").getImage();
                 g.drawImage(aOImage, 0, 0, getHeight(), getHeight(), null);
             }
         }
@@ -175,6 +178,7 @@ class XOGameFrame extends JFrame {
                     whoseTurn = ' ';
                     gameOver = true;
                 } else {
+                    //Change text in status bar. Who must go now.
                     whoseTurn = (whoseTurn == 'X') ? 'O' : 'X';
                     jlStatusBar.setText(whoseTurn + "'s turn.");
                 }
